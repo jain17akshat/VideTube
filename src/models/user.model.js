7f1abac23e1fb6f5926098ebab93aab69b3a74ac
jain17akshat <jain.7akshat@gmail.com>
2025-04-13 12:43:23 +0530

    Refactor user model to include password hashing and token generation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,4 +58,49 @@ refreshToken:{
     timestamps:true,
 }
 )
+
+
+userScehma.pre("save",async function(next){
+if(this.isModified("password"))return next();
+
+this.password=bcrypt.hash(this.password,10)
+next()
+
+})
+
+userScehma.methods.isPasswordCorrect=async function(password) {
+ return await   bcrypt.compare(password,this.password)
+    
+}
+
+userScehma.methods.generateAccessToken=function(){
+   return  jwt.sign(
+    {
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+
+}
+userScehma.methods.generateRefreshToken=function(){
+    return  jwt.sign(
+        {
+            _id:this._id,
+           
+    
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export  const User=mongoose.model("User",userScehma)
