ab2d0bce917c01b6ba04d99849cb27cd5635083f
jain17akshat <jain.7akshat@gmail.com>
2025-04-15 22:21:37 +0530

    Refactor app.js and index.js, and add user controller and user routes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from ("express")
+import express from "express"
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
@@ -13,4 +13,16 @@ app.use(express.urlencoded({extended:true,limit:"32kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
  
+
+
+
+
+//routes
+
+import userRouter from './routes/user.routes.js'
+ 
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+// http://localhost:8000/users/register
 export {app}
